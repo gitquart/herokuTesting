33e6586a4625576bc353ef9992607e6e5836108f
Ulyses Rico <ulyses.rico.2012@gmail.com>
2020-06-05 12:14:29 -0500

    Deploy test...

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
-print('Hello heroku')
+from selenium import webdriver
+import chromedriver_autoinstaller
+
+chromedriver_autoinstaller.install()
+driver=webdriver.Chrome()
+
+driver.get("https://www.google.com")
+
+print(driver.page_source)
